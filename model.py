5f0f13e2a0fd65155840f892b6a774da6caa6d22
rob-crane <rob-crane@users.noreply.github.com>
2018-04-16 22:49:33 -0400

    incorporated preprocessing as model layer, removed ImageDataGenerator

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import os
 import csv
+from random import randint
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, BatchNormalization
+from keras.layers import Flatten, Dense, Conv2D, Lambda
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adadelta
+from keras.backend import mean
 from scipy import misc
 from time import strftime
 from math import ceil
@@ -22,7 +24,9 @@ def get_fname(path):
     else:
         return path.split('\\')[-1]
 
-def get_batch(batch_size, randomize):
+def get_batch(batch_size, h_flip):
+
+    # provide iterator over all images in CSV file
     def _image_gen():
         with open(csv_file) as f:
             reader = csv.reader(f)
@@ -36,30 +40,15 @@ def get_batch(batch_size, randomize):
 
     image_source = _image_gen()
 
-    # Image pre-processing pipeline
-
-    trim_image = lambda image : image # TODO trim portion of sky
-    image_adjuster = ImageDataGenerator(
-            samplewise_center = True,
-            samplewise_std_normalization = True)
-            # horizontal_flip = True)
-            # preprocessing_function = trim_image)
-
-    if randomize:
-        def preprocess(img_batch):
-            normalized = image_adjuster.standardize(img_batch)
-            return image_adjuster.random_transform(normalized)
-    else:
-        def preprocess(img_batch):
-            normalized = image_adjuster.standardize(img_batch)
-            return normalized
-
     while True:
         images = np.empty(shape=(batch_size,) + bc_config.IMG_SHAPE)
         labels = np.empty(shape=(batch_size,))
         try:
             for i in range(batch_size):
                 (image, label) = next(image_source)
+                if h_flip and randint(0,1):
+                    image = np.flip(image, 1)
+                    label = -label
                 images[i] = image
                 labels[i] = label
         except StopIteration:
@@ -67,14 +56,19 @@ def get_batch(batch_size, randomize):
             labels = labels[:i]
             if len(labels) == 0: # if num_examples % batch_size is 0
                 continue
-            yield preprocess(images), labels
+            yield images, labels
             image_source = _image_gen() # reset generator to loop back over
             continue
-        yield preprocess(images), labels
+        yield images, labels
+
+
+def preprocess(x):
+    return x / 255.0 #(x - mean(x, axis=[1,2,3], keepdims=True)) / 255.0
 
 model = Sequential()
 
-model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu', input_shape=bc_config.IMG_SHAPE))
+model.add(Lambda(preprocess, input_shape=bc_config.IMG_SHAPE))
+model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu'))
 model.add(Conv2D(36, (5,5), strides=(2,2), activation='relu'))
 model.add(Conv2D(48, (5,5), strides=(2,2), activation='relu'))
 model.add(Conv2D(64, (3,3), strides=(1,1), activation='relu'))
