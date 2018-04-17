a37f50b9e6152ea6ef5ed40520236ea5f663e7b4
rob-crane <rob-crane@users.noreply.github.com>
2018-04-16 21:06:25 -0400

    Configured from file

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import numpy as np
 from keras.models import Sequential
@@ -5,18 +6,12 @@ from keras.layers import Flatten, Dense, Conv2D, BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adadelta
 from scipy import misc
-from datetime import datetime
+from time import strftime
 from math import ceil
-# import matplotlib.pyplot as plt
+import bc_config
 
-CAM_OFFSET_FACTOR = 0.001
-DIR_PATH = '/home/carnd/CarND-P3-Behavioral-Cloning/data/training_images_2/'
-CSV_FILE = 'driving_log.csv'
-IMAGE_DIRNAME = 'IMG'
-IMG_SHAPE = (160, 320, 3)
-
-csv_file = DIR_PATH + '/' + CSV_FILE
-img_dir = DIR_PATH + '/' + IMAGE_DIRNAME + '/'
+csv_file = bc_config.DATA_DIR + '/' + bc_config.CSV_FILE
+img_dir = bc_config.DATA_DIR + '/' + bc_config.IMAGE_DIRNAME + '/'
 
 num_images = sum(3 for line in open(csv_file))
 
@@ -35,9 +30,9 @@ def get_batch(batch_size, randomize):
                 yield (misc.imread(img_dir + get_fname(line[0])), 
                         float(line[3]))
                 yield (misc.imread(img_dir + get_fname(line[1])), 
-                        float(line[3]) + CAM_OFFSET_FACTOR)
+                        float(line[3]) + bc_config.CAM_OFFSET_FACTOR)
                 yield (misc.imread(img_dir + get_fname(line[2])), 
-                        float(line[3]) - CAM_OFFSET_FACTOR)
+                        float(line[3]) - bc_config.CAM_OFFSET_FACTOR)
 
     image_source = _image_gen()
 
@@ -60,7 +55,7 @@ def get_batch(batch_size, randomize):
             return normalized
 
     while True:
-        images = np.empty(shape=(batch_size,) + IMG_SHAPE)
+        images = np.empty(shape=(batch_size,) + bc_config.IMG_SHAPE)
         labels = np.empty(shape=(batch_size,))
         try:
             for i in range(batch_size):
@@ -79,7 +74,7 @@ def get_batch(batch_size, randomize):
 
 model = Sequential()
 
-model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu', input_shape=IMG_SHAPE))
+model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu', input_shape=bc_config.IMG_SHAPE))
 model.add(Conv2D(36, (5,5), strides=(2,2), activation='relu'))
 model.add(Conv2D(48, (5,5), strides=(2,2), activation='relu'))
 model.add(Conv2D(64, (3,3), strides=(1,1), activation='relu'))
@@ -91,16 +86,12 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
 
-BATCH_SIZE = 128
-VALIDATION_SPLIT = 0.1
-EPOCHS = 5
-
-steps_per_epoch = steps_per_epoch=ceil(num_images / BATCH_SIZE)
-validation_steps = int(VALIDATION_SPLIT * steps_per_epoch)
+steps_per_epoch = steps_per_epoch=ceil(num_images / bc_config.BATCH_SIZE)
+validation_steps = int(bc_config.VALIDATION_SPLIT * steps_per_epoch)
 train_steps = steps_per_epoch - validation_steps
 
-train_source = get_batch(BATCH_SIZE, True)
-test_source = get_batch(BATCH_SIZE, True)
+train_source = get_batch(bc_config.BATCH_SIZE, True)
+test_source = get_batch(bc_config.BATCH_SIZE, True)
 
 print('exp total:', str(steps_per_epoch))
 print('train_steps: ', str(train_steps))
@@ -113,18 +104,31 @@ model.compile(optimizer=optimizer,
 history = model.fit_generator(
         train_source, 
         train_steps, 
-        epochs = EPOCHS, 
+        epochs = bc_config.EPOCHS, 
         validation_data = test_source, 
         validation_steps = validation_steps)
 
-timestamp = str(datetime.now())
-model.save('models/' + timestamp + '.h5')
+timestamp = strftime('%d-%b-%y_%H:%M:%S')
 
-# for i in range(len(LEARN_RATES)):
-    # print(str(LEARN_RATES[i]) + ' === ' + str(val_losses[i]))
-# plt.plot(np.log10(LEARN_RATES), val_losses)
-# plt.title('Model Loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Learn Rate')
-# timestamp = str(datetime.now())
-# plt.savefig('figs/' + timestamp + '.png')
+try:
+    os.mkdir(bc_config.MODEL_DIR)
+except FileExistsError:
+    pass
+
+model.save(bc_config.MODEL_DIR + '/' + timestamp + '.h5')
+
+if bc_config.PLOT_LOSSES:
+    import matplotlib.pyplot as plt
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train Loss', 'Val. Loss'], loc='upper left')
+    try:
+        os.mkdir(bc_config.PLOT_DIR)
+    except FileExistsError:
+        pass
+    plt.savefig(bc_config.PLOT_DIR + '/' + timestamp + '.png')
+print('done')
