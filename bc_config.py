7c2fadff5afe932318221c1e7918d36c9809c5e2
rob-crane <rob-crane@users.noreply.github.com>
2018-04-22 17:10:10 -0400

    Added steering gain value, submission video and example config file

---
+++ bc_config.py
@@ -0,0 +1,15 @@
+CAM_OFFSET_FACTOR = 0.001
+DATA_DIR = '/home/robert/projects/bc_proj/training_images_2/'
+CSV_FILE = 'driving_log.csv'
+IMAGE_DIRNAME = 'IMG'
+IMG_SHAPE = (160, 320, 3)
+
+BATCH_SIZE = 32
+VALIDATION_SPLIT = 0.1
+EPOCHS = 1
+MODEL_DIR = '/home/robert/projects/bc_proj/fits'
+
+PLOT_LOSSES = True
+PLOT_DIR = '/home/robert/projects/bc_proj/plots'
+
+STEER_GAIN = 2.0
